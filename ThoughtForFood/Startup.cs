522e9356819f17ba71cae45461dde2ed2e50b979
Rodney Gillum <rGillum@catalystitservices.com>
2016-05-19 10:24:11 -0400

    Add project files.

---
+++ ThoughtForFood/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThoughtForFood.Startup))]
+namespace ThoughtForFood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
